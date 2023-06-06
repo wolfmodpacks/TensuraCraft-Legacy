e555cfa97884f821b01c7c8d60c04dcd9a6353f8
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-06-06 19:37:02 -0400

    Releasing Early version of TensuraCraft Legacy

---
+++ modpack/overrides/kubejs/server_scripts/daily_quests.js
@@ -0,0 +1,26 @@
+onEvent('player.logged_in', (event) => {
+    if(!event.player.stages.has('reincarnation')) {
+        event.player.stages.add('reincarnation')
+        event.server.runCommand(`/execute as ${event.player.name} run isekai`)
+    }
+    dailyQuests(event)
+})
+
+
+function dailyQuests(events) {
+    const day = new Date().getDay();
+    const questIds = [
+        "4C2D308AF8A22FFB",
+        "70543DE6F4F458FD",
+        "16B072746A32D469",
+        "792AFA4F56728BDC",
+        "073BB34B017F3525",
+        "1844CFE0D6BA57D5",
+        "6A38F29ACA57A018"
+    ];
+
+    if (day >= 0 && day <= 6) {
+        const questId = questIds[day];
+        events.server.runCommand(`/ftbquests change_progress ${events.player.name} complete ${questId}`);
+    }
+}
