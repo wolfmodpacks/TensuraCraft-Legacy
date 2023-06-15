887775a2f9fa9196f4632ef3964316e458d69096
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-06-15 12:09:30 -0400

    New quests rewards / Fixed Quests not being accepted

---
+++ modpack/overrides/kubejs/server_scripts/recipies.js
@@ -0,0 +1,9 @@
+onEvent('recipes', event => {
+    // Furnace
+    event.remove({ id: 'coins:furnace/gold_ingot_to_gold_coin'})
+    event.remove({ id: 'coins:furnace/iron_ingot_to_iron_coin'})
+
+    // Blast Furnace
+    event.remove({ id: 'coins:blasting/gold_ingot_to_gold_coin'})
+    event.remove({ id: 'coins:blasting/iron_ingot_to_iron_coin'})
+})
