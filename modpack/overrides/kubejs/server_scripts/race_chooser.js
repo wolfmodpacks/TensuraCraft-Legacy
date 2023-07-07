f347703cf2ca68797026a1bc3f24b9c6affcf14b
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-07-07 18:41:35 -0400

    Update v0.2.2

--- modpack/overrides/kubejs/server_scripts/race_chooser.js
+++ modpack/overrides/kubejs/server_scripts/race_chooser.js
@@ -1,5 +1,14 @@
 const $FTBRanksAPI = java('dev.ftb.mods.ftbranks.api.FTBRanksAPI')
 
+function removeLastTeleport(player) {
+    const teleportHistory = $FBTEssentials.get(player.getProfile()).teleportHistory
+    if (!teleportHistory.isEmpty()) {
+        teleportHistory.removeLast()
+    }
+
+}
+
+
 function getManager() {
     return $FTBRanksAPI.INSTANCE.getManager()
 }
@@ -19,6 +28,7 @@ onEvent('player.tick', (event) => {
 
     if (event.player.server && event.player.ticksExisted % 90 == 0) {
         try {
+            if (!currentRace) return;
             getManager().getAllRanks().forEach(e => {
                 if (formatedRace == e.getId()) {
                     if (rankAdded(event.player, e)) return;
@@ -44,7 +54,6 @@ onEvent('item.pickup', (event) => {
     }
 
     event.server.scheduleInTicks(10, () => {
-        const spawn = event.player
         const player = event.player
 
         switch (event.item) {
@@ -52,42 +61,62 @@ onEvent('item.pickup', (event) => {
                 player.runCommandSilent('race Goblin')
                 player.runCommandSilent(`clear ${event.player} kubejs:goblin_head`)
                 player.runCommand(`spawn`)
+                removeLastTeleport(event.player)
+
                 break;
             case 'kubejs:lizardman':
                 player.runCommandSilent('race Lizardman')
                 player.runCommandSilent(`clear ${event.player} kubejs:lizardman`)
                 player.runCommand(`spawn`)
+                removeLastTeleport(event.player)
+
                 break;
+
             case 'kubejs:orc_head':
                 player.runCommandSilent('race Orc')
                 player.runCommandSilent(`clear ${event.player} kubejs:orc_head`)
                 // 5% chance of obtaining self_regeneration
                 if (Math.random() < 0.05) player.give(`ttigraas:self_regeneration`)
                 player.runCommand(`spawn`)
+                removeLastTeleport(event.player)
+
                 break;
+
             case 'kubejs:vampire_head':
                 player.runCommandSilent('race Vampire')
                 player.runCommandSilent(`clear ${event.player} kubejs:vampire_head`)
                 player.give(`ttigraas:vampirism`)
                 player.runCommand(`spawn`)
+                removeLastTeleport(event.player)
+
                 break;
+
             case 'kubejs:human_head':
                 player.runCommandSilent('race Human')
                 player.runCommandSilent(`clear ${event.player} kubejs:human_head`)
                 player.runCommand(`spawn`)
+                removeLastTeleport(event.player)
+
                 break;
+
             case 'kubejs:slime':
                 player.runCommandSilent('race Slime')
                 player.runCommandSilent(`clear ${event.player} kubejs:slime`)
                 player.give(`ttigraas:absorb_and_dissolve`)
                 player.give(`ttigraas:self_regeneration`)
                 player.runCommand(`spawn`)
+                removeLastTeleport(event.player)
+
                 break;
+
             case 'kubejs:wight_head':
                 player.runCommandSilent('race Wight')
                 player.runCommandSilent(`clear ${event.player} kubejs:wight_head`)
                 player.runCommand(`spawn`)
+                removeLastTeleport(event.player)
+
                 break;
+
         }
 
     })
