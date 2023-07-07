3699e801a1f52e9b819d5b63a704b533bea4553e
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-07-07 00:41:53 -0400

    Remove Race Reroll

--- modpack/overrides/kubejs/server_scripts/recipes.js
+++ modpack/overrides/kubejs/server_scripts/recipes.js
@@ -6,4 +6,7 @@ onEvent('recipes', event => {
     // Blast Furnace
     event.remove({ id: 'coins:blasting/gold_ingot_to_gold_coin'})
     event.remove({ id: 'coins:blasting/iron_ingot_to_iron_coin'})
+
+    // Ttigraas
+    event.remove({ id: 'ttigraas:race_reroll_scroll'})
 })
